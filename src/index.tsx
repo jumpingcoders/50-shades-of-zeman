45e1094c0f316be18603faf564c5b29888a80b27
Michal Vašíček <me@michalvasicek.cz>
2017-12-17 04:08:08 +0100

    Libuse

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,29 @@
 import Game from './Game';
 import './index.css';
 
-
-
 const game = new Game();
 
-setInterval(()=>{
-    console.log(game.getSpeed());
-},200);
+class Music {
+    public el: HTMLAudioElement;
+
+    constructor() {
+        this.el = new Audio;
+        this.el.src = '/assets/sounds/libuse.mp3';
+        this.el.play();
+    }
+
+    updatePlayerSpeed() {
+        this.el.playbackRate = game.getSpeed();
+    }
+}
+
+let music = new Music;
+
+setInterval(function() {
+    music.updatePlayerSpeed();
+}, 200);
+
+
 
 
 
