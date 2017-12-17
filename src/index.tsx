f05b633646445d27936a3281ec8929ef763c1fc9
hejny <me@pavolhejny.com>
2017-12-17 05:45:11 +0100

    + Music

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,6 @@
 import Game from './Game/';
+import Music from './Music';
 import './index.css';
 
 const game = new Game();
-
-class Music {
-    public el: HTMLAudioElement;
-
-    constructor() {
-        this.el = new Audio;
-        this.el.src = '/assets/sounds/libuse.mp3';
-        this.el.play();
-    }
-
-    updatePlayerSpeed() {
-        let speed = game.musicSpeed;
-        console.log(speed);
-        if(speed<.5)speed=.5;
-        if(speed>3)speed=3;
-        this.el.playbackRate = speed;
-    }
-}
-
-let music = new Music;
-
-setInterval(function() {
-    music.updatePlayerSpeed();
-}, 200);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let music = new Music('libuse',game);
